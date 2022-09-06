6bc058d989216f503dbfcb3b8ea4ee55d1079287
Your Name <you@example.com>
2022-09-06 11:30:55 -0400

    new section

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const menuToggle = document.querySelector('.menu-toggle-bar')
 menuBar.addEventListener('click', (e) => {
     e.preventDefault();
     menuToggle.classList.toggle('toggle')
-}) 
+})
+
+
+
+
+const btn  = document.getElementById('btn')
+const active = document.querySelector('.box')
+
+btn.addEventListener('click', () => {
+    active.classList.toggle('active')
+})
+
